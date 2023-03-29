e3addaa3258d9c50c39c6d462e0039a76c2bd4af
Alex <youdoit@mail.ru>
2023-03-29 14:02:39 +0600

    Added admin panel

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+admin.site.register(EntertainmentsPlace)
+admin.site.register(EntertainmentsEvent)
+admin.site.register(Events)
+admin.site.register(Tours)
+admin.site.register(TourSchedule)
+admin.site.register(ToursImage)
+admin.site.register(FoodPlaceCategory)
+admin.site.register(Cuisine)
+admin.site.register(FoodPlaces)
+admin.site.register(Menu)
